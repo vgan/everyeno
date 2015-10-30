152db7b696b949c67b5121d42fba28ec31eceb47
Steve <github@stevecvar.com>
2015-10-30 17:25:11 -0400

    Create everyeno_keys.py
    Adding example Keys file

---
+++ everyeno_keys.py
@@ -0,0 +1,13 @@
+tumblr_consumer_key = ''
+tumblr_consumer_secret = ''
+tumblr_token_key = ''
+tumblr_token_secret = ''
+
+google_developerKey = ''
+
+twitter_consumer_key = ''
+twitter_consumer_secret =  ''
+twitter_token_key = ''
+twitter_token_secret = ''
+
+discogs_user_token = ''
