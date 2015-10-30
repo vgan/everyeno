2207abb43b769877f4acfb3f6a1887e812991005
Steve Cvar <github@stevecvar.com>
2015-10-30 16:56:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+everyeno
+========
+everyeno is a bot designed to run on a schedule and tweet/ tumbl details of master releases for brian eno in chronological order. </br>
+you can see results here:
+    http://twitter.com/everyeno
+    http://everyeno.tumblr.com
+
+i thought this would be a fun way to learn to use the discogs API and visualize how much interesting work brian eno has been involved with over the years as an artist and producer. </br>
+<b>Operation:</b> </br>
+<ul>
+    <li>on 1st run, it downloads info for every master release brian eno is credited on via the discogs api and creates a local file called releases.csv (note: after first run manually check and sort the data in the csv to your liking before running again.)</li>
+    <li>on subsequent runs it steps through each release and tweets/ tumbls each one, keeping track via a second local file  tweeted.txt</li>
+</ul>
+
+<b>APIs/ Wrappers:</b> </br>
+  - Discogs https://github.com/discogs/discogs_client
+  - Twitter (tweepy) https://github.com/tweepy/tweepy
+  - Tumblr (pytumblr) https://github.com/tumblr/pytumblr
+  - Google Custom Search Engine (cse) https://developers.google.com/custom-search/json-api/v1/reference/cse/list
+</br>
+
+<b>Scheduling via Cron:</b> </br>
+        @hourly your-username python /path-to-your-script/everyeno.py >> /path-to-your-script/everyeno.log 2>&1
+
---
+++ everyeno.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
+import os
+import time
+import discogs_client
+import tweepy
+import urllib
+import mmap
+from apiclient.discovery import build
+import pytumblr
+from everyeno_keys import *
+
+client = pytumblr.TumblrRestClient(tumblr_consumer_key, tumblr_consumer_secret, tumblr_token_key, tumblr_token_secret)
+service = build("customsearch", "v1",developerKey=google_developerKey)
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+auth.set_access_token(twitter_token_key, twitter_token_secret)
+api = tweepy.API(auth)
+discogs = discogs_client.Client('EveryEno/0.2', user_token=discogs_user_token)
+
+results = discogs.search('Brian Eno',type='master') # This search will return all master releases 
+
+basedir = '/home/vgan/everyeno/' # set this to the working directory for the script
+releasesfilepath = basedir + 'releases.csv'
+thumbfile = basedir + 'thumb.jpg'
+thumbobj = 'nope'
+tweetedfilepath = basedir + 'tweeted.txt'
+video_url = 'nope'
+now = time.strftime("%c")
+emoji_discogs = unicode('\xF0\x9F\x93\x96', 'utf-8')
+emoji_video = unicode('\xF0\x9F\x93\xBA', 'utf-8')
+emoji_year =  unicode('\xF0\x9F\x93\x86', 'utf-8')
+emoji_title = unicode('\xF0\x9F\x8E\xB6', 'utf-8')
+
+if not os.path.isfile(tweetedfilepath): # create the tweeted.txt file if it doesnt exist  
+	tweetedfile = open(tweetedfilepath,"w")
+	tweetedfile.write('MASTER_URLS\n') # write a header and close file
+        tweetedfile.close()
+
+def doTweet():
+	if thumbobj == 'nope':
+		try: # no thumbnail just tweet info and link
+                        tweetbody = + emoji_title + ": " + title + "\n" + emoji_year + ": " + year + "\n" + emoji_discogs + ": " + discogs_url + "\n"   
+                        status = api.update_status(status=tweetbody)
+                        print now + ' tweeted without tumbnail: ' + title
+        	except:
+                 	print now + ' no thumbnail tweet failed: ' + title
+	else:
+                try:    # yes thumbnail
+                	tweetbody = emoji_title + ": " + short_title + "\n" + emoji_year + ": " + year + "\n" + emoji_discogs + ": " + discogs_url + "\n" + emoji_video + ": " + video_url   
+                	status = api.update_with_media(thumbfile,status=tweetbody,file=thumbobj)
+                	print now + ' tweeted: ' + title
+                except: 
+                        print now + ' tweet with thumbnail failed: ' + title + "\n"
+
+def doTumblr():  # check if we should do vdeo post or just picture
+        if video_url == 'nope':
+		try:
+			client.create_photo('everyeno', state="published", tags=["Every Eno", "Brian Eno", year, "Discogs API", "Music Geek"], caption=title + "\n" + year + "\n"+ discogs_url + "\n" , link=discogs_url, data=thumbfile)
+			print now + ' tumblrd photo post: ' + title
+		except:
+			print now + " tumblr photo post failed: " + title
+	else:
+		try:
+        		client.create_video('everyeno', state="published", tags=["Every Eno", "Brian Eno", year , "Discogs","Discogs API","Music Geek"], caption=title + "\n" + year + "\n" + discogs_url + "\n", embed=video_url)
+			print now + ' tumblrd video post: ' + title
+        	except:
+        		print now + ' tumblr video post failed: ' + title
+
+if os.path.isfile(releasesfilepath): # releases.csv file exists - do some stuff
+	with open(releasesfilepath) as releasesfile:
+		tweetedfile = open(tweetedfilepath,"a+") # open tweetlog for appending
+		m = mmap.mmap(tweetedfile.fileno(), 0, access=mmap.ACCESS_READ) # memory map the tweetlog
+		for line in releasesfile: # start release loop
+			release = line.split(",")
+			year = str(release[0])
+			title = str(release[1])
+			discogs_url = release[2]
+			if m.find(discogs_url) == -1: # master url has not been tweeted yet
+				try: # download thumbnail
+                                	thumb = release[3]
+                                	urllib.urlretrieve(thumb, thumbfile)
+                                	thumbobj = open(thumbfile)
+                       		except:
+                                	thumbobj = 'nope'
+				try:
+					res = service.cse().list( # google the title for first result on youtube
+        	        			q=str(title),
+        	        			cx="018071961790225090184:6k5fcoxbomq", # my custom search limited to youtube.com/watch* - set up your own here: http://www.google.com/cse/manage/all 
+        	        			num=1,
+        	        			start=1,
+        				).execute()
+
+        				if not 'items' in res:
+						video_url = 'nope' # no results, set to empty string
+        				else:
+                				for item in res['items']:
+                        				video_url = format(item['link'])
+				except:
+					print now + ' google search failed.'
+					video_url = 'nope'
+
+				if len(title) >= 48:  # truncate title to first 48 characters + elipses (this is the space leftover from image + 2 URLs, emoji, etc..)
+					short_title = title[:48] + "..." 
+				else:
+					short_title = title
+
+				doTweet()
+				doTumblr()
+				tweetedfile.write(release[2] + '\n') # write master url to tweeted file
+				print now + ' cleaning up and exiting script...'
+                                tweetedfile.close() # close tweeted.txt file
+				os.remove(thumbfile)# delete thumbnail file
+				quit() 
+			#else:
+				# print 'already tweeted this one, skipping ' + title 
+
+else: # no releases.csv file - lets make one
+	totalreleases = len(results) # might be able to use this later for comparing with local file and downloading updates?
+	print('total releases: ' + str(totalreleases))
+	releasesfile = open(releasesfilepath,"w") # create the file since it doesnt exist	
+        releasesfile.close()
+	releasesfile = open(releasesfilepath,"a+") # open for appending
+	for result in results:
+		#print result.data.keys()
+		title = result.title.encode('ascii',errors='ignore') # encode to ascii to clean up weird characters in title, will still require some cleanup 
+		title = title.replace(',', '-') # better replace commas since we are using as delimeter 
+		try: 
+			URL = result.data['uri']
+			URL = 'http://www.discogs.com' + URL
+		except:
+			print now + ' error grabbing release.master.id'
+		try:
+ 			year = str(result.data['year'])
+		
+		except:
+			print now + ' error getting year'
+			year = "0"
+		thumb = result.data['thumb']
+		releasesfile.write(year + ',' + title + ',' + URL + ',' + thumb + '\n')
+	 	time.sleep(1)	
+	releasesfile.close()
+	print now + ' releases.csv has been created. you should manually clean it up / sort it before running again - i was missing lots of years for releases in my case...'
